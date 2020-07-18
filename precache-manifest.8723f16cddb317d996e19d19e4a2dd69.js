e99a04ca0be98814fd0944cb87db52653463e111
Adekunle Bamikole <berkel97@gmail.com>
2020-07-18 21:14:28 +0100

    Updates

---
+++ precache-manifest.8723f16cddb317d996e19d19e4a2dd69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87ae40ec2e730bef57b8172f73daabf",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "409eefaea3e0380cd1bb",
+    "url": "/todoapp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fa8615706205959af86e",
+    "url": "/todoapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "409eefaea3e0380cd1bb",
+    "url": "/todoapp/static/js/2.e8adbf0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.e8adbf0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8615706205959af86e",
+    "url": "/todoapp/static/js/main.61daaec8.chunk.js"
+  },
+  {
+    "revision": "5a801258d7bf2526530b",
+    "url": "/todoapp/static/js/runtime-main.3e0c5687.js"
+  }
+]);
